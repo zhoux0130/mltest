4129d2fdfe193918fd20c288923f93f6f95b25ad
周雪 <zhouxue@quancheng-ec.com>
2017-02-15 14:41:33 +0800

    添加皮尔逊相似度实现

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example;
 
 import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,6 +19,8 @@ public class DemoApplication {
 		String originalData = "{'apple':{'A': 3.4,'B': 2.5, 'C': 4.4, 'D':3.8},'kiwi':{'A': 3.9,'E': 4.5, 'C': 3.4, 'D':4.0},'peach':{'A': 2.4,'B': 2.7, 'C': 4.1, 'D':3.8, 'E':4.2}}";
 		Map originalMap = (Map) JSON.parse(originalData);
 		simDistance(originalMap, "apple", "peach");
+
+		sortPerson(4, "apple",originalMap);
 	}
 
 	// 计算任意两个人的欧几米的距离，并获得对应的相似系数
@@ -43,25 +44,83 @@ public class DemoApplication {
 		return 1/(1+Math.pow(distance2Person, 0.5));
 	}
 
+	// 通过皮尔逊方式，计算两个人的相似度
+	public static double simDistance1(Map data, String thePerson, String person){
+		Map thePersonData = (Map) data.get(thePerson);
+		Map personData = (Map) data.get(person);
+
+		// 获取两个不同的人，对同一部电影的评分，并且将他们相乘,再将所有的结果相加
+		double xPowSum = 0.0;
+		double yPowSum = 0.0;
+		double mulSum = 0.0;
+		double xSum = 0.0;
+		double ySum = 0.0;
+
+		int n = 0;
+
+		for(Object key: thePersonData.keySet()){
+			BigDecimal xObj = (BigDecimal) thePersonData.get(key);
+			BigDecimal yObj = (BigDecimal) thePersonData.get(key);
+
+			double x = xObj.doubleValue();
+			double y = 0.0;
+
+			if(yObj != null){
+				y = yObj.doubleValue();
+				n ++;
+			}
+
+			xPowSum += Math.pow(x, 2);
+			yPowSum += Math.pow(y, 2);
+			mulSum += (x*y);
+			xSum += x;
+			ySum += y;
+
+		}
+
+		double a = mulSum - (xSum * ySum)/n;
+		double b = Math.pow((xPowSum - xPowSum/n)*(yPowSum - yPowSum/n), 0.5);
+		double r = 1 + a/b;
+
+		return r;
+	}
+
 	// 对相关的人进行相似度打分，获取对应的排序
-	public List<Map> sortPerson(int rank, String person, Map data){
-		List rankList = new ArrayList();
+	public static List<Map.Entry<String, Double>> sortPerson(int rank, String person, Map data){
+		Map personScore = new HashMap();
 		for(Object key : data.keySet()){
 			// 其他人的近似分数
 			if(!StringUtils.equals(key.toString(), person)){
 				String personName = String.valueOf(key);
 				double simValue = simDistance(data, person, personName);
-				rankList.add(simValue);
+				personScore.put(key, simValue);
 			}
 		}
 
-		Collections.sort(rankList);
-		if(rank < 1){
+		Comparator<Map.Entry> valueComparator = new Comparator<Map.Entry>() {
+			@Override
+			public int compare(Map.Entry o1, Map.Entry o2) {
+				Double v1 = (Double) o1.getValue();
+				Double v2 = (Double) o2.getValue();
+				return v1 - v2 > 0 ? 1 : 0;
+			}
+		};
+
+
+		List<Map.Entry<String, Double>> list = new ArrayList<Map.Entry<String,Double>>(personScore.entrySet());
+
+		Collections.sort(list, valueComparator);
+		if(rank < 1 ){
 			return null;
 		}
-//		rankList.subList(0, )
+		list.forEach( d -> System.out.println(d));
 
-		return null;
+		if(rank  > list.size()){
+			return list;
+		}
+		list.subList(0, rank-1);
+
+		return list;
 	}
 
 }
