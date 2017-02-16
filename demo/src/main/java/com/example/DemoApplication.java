09276dc4c1811cc31944578071137f1240996ed5
周雪 <zhouxue@quancheng-ec.com>
2017-02-16 10:53:58 +0800

    add

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -11,116 +11,127 @@ import java.util.*;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
 
-		// 获取原始数据，将其转换成对应的数据结构
-		String originalData = "{'apple':{'A': 3.4,'B': 2.5, 'C': 4.4, 'D':3.8},'kiwi':{'A': 3.9,'E': 4.5, 'C': 3.4, 'D':4.0},'peach':{'A': 2.4,'B': 2.7, 'C': 4.1, 'D':3.8, 'E':4.2}}";
-		Map originalMap = (Map) JSON.parse(originalData);
-		simDistance(originalMap, "apple", "peach");
+        // 获取原始数据，将其转换成对应的数据结构
+        //String originalData = "{'apple':{'A': 3.4,'B': 2.5, 'C': 4.4, 'D':3.8},'kiwi':{'A': 3.9,'E': 4.5, 'C': 3.4, 'D':4.0},'peach':{'A': 2.4,'B': 2.7, 'C': 4.1, 'D':3.8, 'E':4.2}}";
+        String critics = "{'Lisa Rose':{'Lady in the Water':2.5,'Snakes on a Plane':3.5,'Just My Luck':3.0,'Superman Returns':3.5,'You, Me and Dupree':2.5,'The Night Listener':3.0},'Gene Seymour':{'Lady in the Water':3.0,'Snakes on a Plane':3.5,'Just My Luck':1.5,'Superman Returns':5.0,'You, Me and Dupree':3.5,'The Night Listener':3.0},'Micheal Phillips':{'Lady in the Water':2.5,'Snakes on a Plane':3.0,'Superman Returns':3.5,'The Night Listener':4.0},'Claudia Puig':{'Snakes on a Plane':3.5,'Just My Luck':3.0,'Superman Returns':4.0,'You, Me and Dupree':2.5,'The Night Listener':4.5},'Mick LaSalle':{'Lady in the Water':3.0,'Snakes on a Plane':4.0,'Just My Luck':2.0,'Superman Returns':3.0,'You, Me and Dupree':2.0,'The Night Listener':3.0},'Jack Matthews':{'Lady in the Water':3.0,'Snakes on a Plane':4.0,'Superman Returns':5.0,'You, Me and Dupree':3.5,'The Night Listener':3.0},'Toby':{'Snakes on a Plane':4.5,'Superman Returns':4.0,'You, Me and Dupree':1.0}}";
+        Map originalMap = (Map) JSON.parse(critics);
+        simDistance(originalMap, "Lisa Rose", "Gene Seymour");
+        simDistance1(originalMap, "Lisa Rose", "Gene Seymour");
 
-		sortPerson(4, "apple",originalMap);
-	}
+//		sortPerson(4, "apple",originalMap);
+    }
 
-	// 计算任意两个人的欧几米的距离，并获得对应的相似系数
-	private static double simDistance(Map originalData, String person1, String person2){
-		Map person1Map = (Map) originalData.get(person1);
-		Map person2Map = (Map) originalData.get(person2);
+    // 计算任意两个人的欧几米的距离，并获得对应的相似系数
+    // Checked
+    private static double simDistance(Map originalData, String person1, String person2) {
+        Map person1Map = (Map) originalData.get(person1);
+        Map person2Map = (Map) originalData.get(person2);
 
-		double distance2Person = 0.0;
-		//TODO: Java8 语法改造
-		for(Object attribute : person1Map.keySet()){
+        double distance2Person = 0.0;
 
-			BigDecimal valueOfPerson1 = ((BigDecimal) person1Map.get(attribute));
-			BigDecimal valueOfPerson2 = null;
-			if(person2Map.containsKey(attribute)){
-				valueOfPerson2 = (BigDecimal) person2Map.get(attribute);
-				distance2Person += Math.abs(Math.pow(valueOfPerson2.doubleValue(), 2) - Math.pow(valueOfPerson1.doubleValue(), 2));
-			}
-		}
+        for (Object attribute : person1Map.keySet()) {
+            BigDecimal valueOfPerson1 = ((BigDecimal) person1Map.get(attribute));
+            BigDecimal valueOfPerson2 = null;
+            if (person2Map.containsKey(attribute)) {
+                valueOfPerson2 = (BigDecimal) person2Map.get(attribute);
+                // 两者之间的差异，再平方
+                distance2Person += Math.pow(valueOfPerson2.doubleValue() - valueOfPerson1.doubleValue(), 2);
+            }
+        }
 
-		System.out.println(1/(1+Math.pow(distance2Person, 0.5)));
-		return 1/(1+Math.pow(distance2Person, 0.5));
-	}
+//		System.out.println(1/(1+Math.pow(distance2Person, 0.5)));
+        return 1 / (1 + Math.pow(distance2Person, 0.5));
+    }
 
-	// 通过皮尔逊方式，计算两个人的相似度
-	public static double simDistance1(Map data, String thePerson, String person){
-		Map thePersonData = (Map) data.get(thePerson);
-		Map personData = (Map) data.get(person);
+    // 通过皮尔逊方式，计算两个人的相似度
+    public static double simDistance1(Map data, String thePerson, String person) {
+        Map thePersonData = (Map) data.get(thePerson);
+        Map personData = (Map) data.get(person);
 
-		// 获取两个不同的人，对同一部电影的评分，并且将他们相乘,再将所有的结果相加
-		double xPowSum = 0.0;
-		double yPowSum = 0.0;
-		double mulSum = 0.0;
-		double xSum = 0.0;
-		double ySum = 0.0;
+        // 获取两个不同的人，对同一部电影的评分，并且将他们相乘,再将所有的结果相加
+        double xPowSum = 0.0;
+        double yPowSum = 0.0;
+        double mulSum = 0.0;
+        double xSum = 0.0;
+        double ySum = 0.0;
 
-		int n = 0;
+        int n = 0;
 
-		for(Object key: thePersonData.keySet()){
-			BigDecimal xObj = (BigDecimal) thePersonData.get(key);
-			BigDecimal yObj = (BigDecimal) thePersonData.get(key);
+        for (Object key : thePersonData.keySet()) {
+            BigDecimal xObj = (BigDecimal) thePersonData.get(key);
+            BigDecimal yObj = (BigDecimal) thePersonData.get(key);
 
-			double x = xObj.doubleValue();
-			double y = 0.0;
+            double x = xObj.doubleValue();
+            double y = 0.0;
 
-			if(yObj != null){
-				y = yObj.doubleValue();
-				n ++;
-			}
+            if (yObj != null) {
+                y = yObj.doubleValue();
+                n++;
+            }
 
-			xPowSum += Math.pow(x, 2);
-			yPowSum += Math.pow(y, 2);
-			mulSum += (x*y);
-			xSum += x;
-			ySum += y;
+            xPowSum += Math.pow(x, 2);
+            yPowSum += Math.pow(y, 2);
+            mulSum += (x * y);
+            xSum += x;
+            ySum += y;
 
-		}
+        }
 
-		double a = mulSum - (xSum * ySum)/n;
-		double b = Math.pow((xPowSum - xPowSum/n)*(yPowSum - yPowSum/n), 0.5);
-		double r = 1 + a/b;
+        if (n == 0) {
+            n = 1; // 防止n作为分母出错
+        }
+        double a = mulSum - (xSum * ySum) / n;
+        // TODO：协方差公式理解
+        double b = Math.pow((xPowSum - Math.pow(xSum, 2) / n) * (yPowSum - Math.pow(ySum, 2) / n), 0.5);
+        double r = a / b;
+        if (b == 0) return 0;
 
-		return r;
-	}
-
-	// 对相关的人进行相似度打分，获取对应的排序
-	public static List<Map.Entry<String, Double>> sortPerson(int rank, String person, Map data){
-		Map personScore = new HashMap();
-		for(Object key : data.keySet()){
-			// 其他人的近似分数
-			if(!StringUtils.equals(key.toString(), person)){
-				String personName = String.valueOf(key);
-				double simValue = simDistance(data, person, personName);
-				personScore.put(key, simValue);
-			}
-		}
-
-		Comparator<Map.Entry> valueComparator = new Comparator<Map.Entry>() {
-			@Override
-			public int compare(Map.Entry o1, Map.Entry o2) {
-				Double v1 = (Double) o1.getValue();
-				Double v2 = (Double) o2.getValue();
-				return v1 - v2 > 0 ? 1 : 0;
-			}
-		};
+        System.out.println(r);
 
 
-		List<Map.Entry<String, Double>> list = new ArrayList<Map.Entry<String,Double>>(personScore.entrySet());
+        return r;
+    }
 
-		Collections.sort(list, valueComparator);
-		if(rank < 1 ){
-			return null;
-		}
-		list.forEach( d -> System.out.println(d));
+    // 对相关的人进行相似度打分，获取对应的排序
+    public static List<Map.Entry<String, Double>> sortPerson(int rank, String person, Map data) {
+        Map personScore = new HashMap();
+        for (Object key : data.keySet()) {
+            // 其他人的近似分数
+            if (!StringUtils.equals(key.toString(), person)) {
+                String personName = String.valueOf(key);
+                double simValue = simDistance(data, person, personName);
+                personScore.put(key, simValue);
+            }
+        }
 
-		if(rank  > list.size()){
-			return list;
-		}
-		list.subList(0, rank-1);
+        Comparator<Map.Entry> valueComparator = new Comparator<Map.Entry>() {
+            @Override
+            public int compare(Map.Entry o1, Map.Entry o2) {
+                Double v1 = (Double) o1.getValue();
+                Double v2 = (Double) o2.getValue();
+                return v1 - v2 > 0 ? 1 : 0;
+            }
+        };
 
-		return list;
-	}
+
+        List<Map.Entry<String, Double>> list = new ArrayList<Map.Entry<String, Double>>(personScore.entrySet());
+
+        Collections.sort(list, valueComparator);
+        if (rank < 1) {
+            return null;
+        }
+        list.forEach(d -> System.out.println(d));
+
+        if (rank > list.size()) {
+            return list;
+        }
+        list.subList(0, rank - 1);
+
+        return list;
+    }
 
 }
